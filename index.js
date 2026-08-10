25a008eb612c218301d88df2670d02b7db253152
Ryuk-god <vickyyadav7702@gmail.com>
2026-08-10 12:55:58 +0000

    10 Aug 2026, 18:25

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import connectDB from './db/connectDB.js';
-import {insertManyDoc } from "./models/movie.js";
+import {allDoc } from "./models/movie.js";
 const app=express();
 const port= process.env.PORT || 8000;
 const Database_URL= process.env.Database_URL || 'mongodb://127.0.0.1:27017/movies';
 
 connectDB(Database_URL);
 
-insertManyDoc();
+allDoc();
 
 app.listen(port ,()=>console.log(`Server listening on port ${port}`));
--- models/movie.js
+++ models/movie.js
@@ -31,67 +31,79 @@ Comments:[{value:{type:String},published:{type:Date,default:Date.now}}],
 // creating model
 const MovieModel =mongoose.model('Movie',movieSchema);
 
-const insertManyDoc = async()=>{
+
+// inserting into the doc or make mongoose use to deploy the data
+// const insertManyDoc = async()=>{
+//   try {
+//     const m1 = new MovieModel({
+//   name: "Extraction 2",
+//   ratings: 4,
+//   money: 60000,
+//   genre: ["action", "adventure"],
+//   isActive: true,
+//   comments: [
+//     { value: "That was an amazing movie." }
+//   ]
+// });
+
+// const m2 = new MovieModel({
+//   name: "John Wick Chapter 4",
+//   ratings: 5,
+//   money: 23000,
+//   genre: ["action"],
+//   isActive: true,
+//   comments: [
+//     { value: "John Wick was amazing!" }
+//   ]
+// });
+
+// const m3 = new MovieModel({
+//   name: "The Dark Knight",
+//   ratings: 5,
+//   money: 55000,
+//   genre: ["action", "crime", "drama"],
+//   isActive: true,
+//   comments: [
+//     { value: "One of the best superhero movies." }
+//   ]
+// });
+
+// const m4 = new MovieModel({
+//   name: "Interstellar",
+//   ratings: 5,
+//   money: 45000,
+//   genre: ["sci-fi", "adventure", "drama"],
+//   isActive: true,
+//   comments: [
+//     { value: "The story and visuals were incredible." }
+//   ]
+// });
+
+// const m5 = new MovieModel({
+//   name: "The Equalizer 3",
+//   ratings: 4,
+//   money: 30000,
+//   genre: ["action", "thriller"],
+//   isActive: false,
+//   comments: [
+//     { value: "Good action movie with a great ending." }
+//   ]
+// });
+// // const result = await m1.save(); // for only save one 
+//   const result = await MovieModel.insertMany([m1,m2,m3,m4]);
+//   console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// } 
+
+
+// iterative the data from the doc
+const allDoc=async ()=>{
   try {
-    const m1 = new MovieModel({
-  name: "Extraction 2",
-  ratings: 4,
-  money: 60000,
-  genre: ["action", "adventure"],
-  isActive: true,
-  comments: [
-    { value: "That was an amazing movie." }
-  ]
-});
-
-const m2 = new MovieModel({
-  name: "John Wick Chapter 4",
-  ratings: 5,
-  money: 23000,
-  genre: ["action"],
-  isActive: true,
-  comments: [
-    { value: "John Wick was amazing!" }
-  ]
-});
-
-const m3 = new MovieModel({
-  name: "The Dark Knight",
-  ratings: 5,
-  money: 55000,
-  genre: ["action", "crime", "drama"],
-  isActive: true,
-  comments: [
-    { value: "One of the best superhero movies." }
-  ]
-});
-
-const m4 = new MovieModel({
-  name: "Interstellar",
-  ratings: 5,
-  money: 45000,
-  genre: ["sci-fi", "adventure", "drama"],
-  isActive: true,
-  comments: [
-    { value: "The story and visuals were incredible." }
-  ]
-});
-
-const m5 = new MovieModel({
-  name: "The Equalizer 3",
-  ratings: 4,
-  money: 30000,
-  genre: ["action", "thriller"],
-  isActive: false,
-  comments: [
-    { value: "Good action movie with a great ending." }
-  ]
-});
-// const result = await m1.save(); // for only save one 
-  const result = await MovieModel.insertMany([m1,m2,m3,m4]);
-  console.log(result);
+    const result = await MovieModel.find();
   } catch (error) {
     console.log(error);
   }
-} 
-export {insertManyDoc};
+}
+export {allDoc};
