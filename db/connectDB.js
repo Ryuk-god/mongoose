2bad65826e0e81f78f95000514c45e28865ebbf6
Ryuk-god <vickyyadav7702@gmail.com>
2026-08-10 17:29:37 +0530

    first commit

---
+++ db/connectDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB=async(DatabaseDb_URL)=>{
+  try {
+    await mongoose.connect(DatabaseDb_URL);
+    console.log("Database Connect...");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default connectDB
---
+++ models/movie.js
@@ -0,0 +1,34 @@
+import { dateTimeValue } from "docx";
+import mongoose from "mongoose";
+
+// creating schema
+const movieSchema=new mongoose.Schema({
+name:{
+  type:String,
+  required:true,
+  trim:true
+},
+rating : {
+  type:Number,
+  required:true,
+  min:0,
+  max:5
+},
+money:{
+  type:mongoose.Decimal128,
+  required:true,
+  validate:(v)=> v>=10
+},
+genre:{
+  type:Array
+},
+isActive:{
+type:Boolean
+},
+Comment:[{value:{type:String},published:{type:Date,default:Date.now}}],
+});
+
+// creating model
+const movieModel =mongoose.model('Movie',movieSchema);
+
+export default movieModel;
