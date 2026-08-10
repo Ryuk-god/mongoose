25fa9d24f82da4c9c4438bc33f766fc2f45ac2b9
Ryuk-god <vickyyadav7702@gmail.com>
2026-08-10 13:51:06 +0000

    10 Aug 2026, 19:21

--- models/movie.js
+++ models/movie.js
@@ -129,7 +129,10 @@ const allDoc=async ()=>{
     // const result =await MovieModel.updateMany({ratings:4},{ratings:3}); // multiple update
 
     // delete
-    const result =await MovieModel.findByIdAndDelete({_id:"6a79c8822d8b85b0320b71fc"})
+    // const result =await MovieModel.findByIdAndDelete("6a79c8822d8b85b0320b71fe") // by id
+    //const result =await MovieModel.findByIdAndDelete({_id:"6a79c8822d8b85b0320b71fe"}) // by id in object
+    // const result = await MovieModel.deleteOne({name:"Interstellar"}); // delete one data 
+    const result = await MovieModel.deleteMany({rating:4});
     console.log(result);
   } catch (error) {
     console.log(error);
